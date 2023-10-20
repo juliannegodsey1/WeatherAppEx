4041c78d479a084ac21312dcd7d39016d33661db
juliannegodsey1 <juliannegodseyy@gmail.com>
2023-10-20 15:47:34 -0400

    final commit

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
 
 namespace WeatherApp
 {
@@ -6,7 +8,18 @@ namespace WeatherApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please enter your zipcode");
+            var zipCode = Console.ReadLine();
+
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+
+            Console.WriteLine();
+
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
         }
     }
 }
